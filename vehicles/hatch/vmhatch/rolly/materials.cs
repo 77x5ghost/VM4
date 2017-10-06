f4bcfda5e67491ce016b373e9de78ca9640ca776
77x5ghost <77x5ghost@gmail.com>
2017-10-06 18:20:23 -0300

    forgot to put the fixed configs
    goddamnit

---
+++ vehicles/hatch/vmhatch/rolly/materials.cs
@@ -0,0 +1,9 @@
+
+singleton Material(Covet_Material_001)
+{
+   mapTo = "Material_001";
+   diffuseColor[0] = "0.639999986 0.639999986 0.639999986 1";
+   specular[0] = "0.25 0.25 0.25 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
